4c251ddfc1dc35e7baf575591640cca1c341b3f7
Kirill Novik <jimmynovik@gmail.com>
2016-05-24 00:27:03 +0000

    Resolved the Synchronous Execution Problem

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,27 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var inject = require('gulp-inject');
+var Q = require('q');
 var mainBowerFiles = require('main-bower-files');
 
 gulp.task('dist-assets', function () {
-  return gulp.src(['./src/**/*.js', './src/**/*.css'].concat(mainBowerFiles()))
+  return gulp.src(['./src/**/**/*.js', './src/**/**/*.css'].concat(mainBowerFiles()))
                 .pipe(gulp.dest('./dist/assets'));
 });
 
-gulp.task('inject', function () {
+gulp.task('inject', ['dist-assets'], function () {
   var target = gulp.src('./src/index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths: 
-  var sources = gulp.src(['./dist/**/**/*.js', './dist/**/**/*.css'], {read: false});
- 
+  var sources = gulp.src(['./dist/assets/**/*.js', './dist/assets/**/*.css'], {read: false});
   return target.pipe(inject(sources))
     .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('clean', function() {
+    return gulp.src('./dist', {read: false})
+		.pipe(clean());
+});
 
 
-gulp.task('default', ['dist-assets', 'inject']);
+
+gulp.task('default', ['clean', 'dist-assets', 'inject']);
